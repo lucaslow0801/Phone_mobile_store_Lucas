a12b93e7d97fa61983375410d25de4f7a6bd869d
Lucaslow <lucaslow0801@gmail.com>
2026-07-10 19:59:28 +0800

    update supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = window.SUPABASE_URL || 'https://YOUR_PROJECT_REF.supabase.co';
-window.SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
+window.SUPABASE_URL = window.SUPABASE_URL || 'https://nintyzxmsievwiwmvhlm.supabase.co';
+window.SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'sb_publishable_Jzb0iqxjguX8CSpNH3ICOQ__q_uu8SE';
